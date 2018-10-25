dd591f86415a02633fd8a7e135c50cfab0ac8e39
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2018-10-25 14:33:50 +0200

    Add comment to keys.template.js

--- config/keys.template.js
+++ config/keys.template.js
@@ -1,4 +1,5 @@
+// Copy this as keys.js and change path to database with correct user and password
 module.exports = {
-  mongoURI:
-    "mongodb://<dbuser>:<dbpassword>@ds255970.mlab.com:55970/c2ba-devconnector"
+    mongoURI:
+        "mongodb://<dbuser>:<dbpassword>@ds255970.mlab.com:55970/c2ba-devconnector"
 };
