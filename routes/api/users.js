e390e18d0ec9b2b090f7bb7e767eecdc589ba9ae
Laurent NOEL <laurent.noel.c2ba@gmail.com>
2018-10-30 14:19:30 +0100

    Add User model and registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,12 @@
 // User management, authentication, etc
 
 const express = require("express");
-
 const router = express.Router();
+
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -12,5 +16,32 @@ router.get("/test", (req, res) =>
         msg: "Users Works"
     })
 );
+
+router.post("/register", async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+    } else {
+        const avatar = gravatar.url(req.body.email, {
+            s: "200", // size
+            r: "pg", // Rating
+            d: "mm" // Default
+        });
+
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            avatar,
+            password: bcrypt.hashSync(req.body.password, 10)
+        });
+
+        try {
+            const user = await newUser.save();
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+});
 
 module.exports = router;
